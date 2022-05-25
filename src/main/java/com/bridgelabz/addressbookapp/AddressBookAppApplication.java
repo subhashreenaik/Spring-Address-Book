2018d06d4ac6270807ad4dc16c43e25fca6f9449
Subhashree naik <subhashree.naik@gmail.com>
2022-05-26 00:04:13 +0530

    Uc2 added

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -2,8 +2,9 @@ package com.bridgelabz.addressbookapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class AddressBookAppApplication {
 
 	
